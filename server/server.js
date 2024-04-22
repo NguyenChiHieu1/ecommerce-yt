ced8ddee7d1ea2580d36024cd39fb44963fdff69
Nguyen Chi Hieu <chihieunc99@gmail.com>
2024-04-22 16:02:45 +0700

    #2

--- server/server.js
+++ server/server.js
@@ -1,12 +1,16 @@
 const express = require('express')
 require('dotenv').config()
+const dbConnect = require('./config/dbconnect')
+const initRoutes = require('./routes')
+const { init } = require('./models/userModel')
 
 const app = express()
-const port = process.env.PORT || 8000
+const port = process.env.PORT || 8888
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+dbConnect()
 
-app.use('/', (req, res) => { res.send('SERVER ONNNNN') })
+initRoutes(app)
 app.listen(port, () => {
     console.log('Server running on th port: ' + port)
 })
