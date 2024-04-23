4aaa96b1712b4f328880d79697a0bab32c29ee39
NguyenChiHieu1 <chihieunc99@gmail.com>
2024-04-23 21:40:13 +0700

    #3 hash password and handler error

---
+++ server/middlewares/errHandler.js
@@ -0,0 +1,21 @@
+const notFound = (req, res, next) => {
+    const error = new Error(`Route ${req.originalUrl} not found!`)
+    res.status(404)
+    next(error)
+}
+
+//để cái next mới xài dc
+const errHandler = (error, req, res, next) => {
+    const statusCode = res.statusCode === 200 ? 500 : res.statusCode
+    return res.status(statusCode).json({
+        success: false,
+        mes: error?.message
+    })
+}
+
+module.exports = {
+    notFound,
+    errHandler
+}
+
+// công dụng chính là giúp rút gọn cái lỗi error khi mà code api có vấn đề, và để tránh hiện ra terminal
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,12 @@
 const userRouter = require('./userRouter')
-
+const { notFound, errHandler } = require('../middlewares/errHandler')
 const initRoutes = (app) => {
     app.use('/api/user', userRouter)
+
+
+    app.use(notFound)
+    //hứng các lỗi api trên để thk dưới hứng: nhờ thk asyncHandler
+    app.use(errHandler)
 }
 
 module.exports = initRoutes
